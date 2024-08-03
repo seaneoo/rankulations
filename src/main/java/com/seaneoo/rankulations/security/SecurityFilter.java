50894a442ab86c4f733e5b72d2e5c5e7bc328aab
Sean O'Connor <so@seano.dev>
2024-08-03 16:56:22 -0600

    Clean up

--- src/main/java/com/seaneoo/rankulations/security/SecurityFilter.java
+++ src/main/java/com/seaneoo/rankulations/security/SecurityFilter.java
@@ -6,11 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -23,6 +24,10 @@ public class SecurityFilter {
     @Autowired
     private JwtFilter jwtFilter;
 
+    // Any routes that do not require authentication to go to
+    private static final List<String> PERMITTED_MATCHERS = List.of("favicon.ico", "/h2-console/**", "/error/**",
+            "/oauth2/**");
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         //noinspection CodeBlock2Expr
@@ -30,18 +35,22 @@ public class SecurityFilter {
                 .headers(headers -> {
                     headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable);
                 })
-                .authorizeHttpRequests(httpRequests -> {
-                    httpRequests.requestMatchers("favicon.ico", "/error/**", "/h2-console/**", "/oauth2/**",
-                                    "/login/**", "/logout/**")
+                .authorizeHttpRequests(matcherRegistry -> {
+                    matcherRegistry.requestMatchers(PERMITTED_MATCHERS.toArray(new String[0]))
                             .permitAll()
                             .anyRequest()
                             .authenticated();
                 })
-                .oauth2Login(oauth -> oauth.successHandler(customAuthSuccessHandler))
-                .formLogin(FormLoginConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
-                .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(
-                        new Http403ForbiddenEntryPoint()))
+                .oauth2Login(oauth -> {
+                    oauth.successHandler(customAuthSuccessHandler);
+                })
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> {
+                    session.sessionCreationPolicy(STATELESS);
+                })
+                .exceptionHandling(exceptionHandling -> {
+                    exceptionHandling.authenticationEntryPoint(new Http403ForbiddenEntryPoint());
+                })
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
