b7de69ddd79eacdaad571eeeca7ce9303d3834ad
Sean O'Connor <so@seano.dev>
2024-08-03 10:35:08 -0600

    Create route to delete a user account

--- src/main/java/com/seaneoo/rankulations/user/UserController.java
+++ src/main/java/com/seaneoo/rankulations/user/UserController.java
@@ -1,7 +1,10 @@
 package com.seaneoo.rankulations.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/me")
     public ResponseEntity<User> me(Authentication authentication) {
         var user = (User) authentication.getPrincipal();
         return ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/delete-account")
+    public ResponseEntity<Object> deleteAccount(Authentication authentication) {
+        var user = (User) authentication.getPrincipal();
+        userRepository.delete(user);
+        return ResponseEntity.status(HttpStatus.GONE)
+                .build();
     }
 }
